e3da58bdf7d03a2fa345fdc31fe86eb0439e8baa
shakthivel <sakthivel2003al@gmail.com>
2025-11-27 16:13:53 +0530

    folder structure mess

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+import prettier from 'eslint-plugin-prettier'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -8,11 +10,18 @@ const eslintConfig = defineConfig([
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
-]);
+  prettier,
+  eslintConfigPrettier,
+  {
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+])
 
-export default eslintConfig;
+export default eslintConfig
